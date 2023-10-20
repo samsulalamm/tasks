8ba4469b4811b1a07c70498a0446043c358f874e
msamsulalam <msamsulalam@gmail.com>
2023-10-20 21:59:27 +0600

    Tasks updated

--- Task 2/task2.ts
+++ Task 2/task2.ts
@@ -1,16 +1,16 @@
-import { Queue } from 'queue-library'; // Use a queue library of your choice
-import { AlertManager } from './alertManager'; // Define your AlertManager class
+import { Queue } from 'queue-library'; // Use a queue library
+import { AlertManager } from './alertManager'; // Defining  AlertManager class
 
-// Define the Transaction interface to represent a transaction.
+// Defining Transaction interface to represent a transaction.
 interface Transaction {
   syndicateId: string;
   amount: number;
   timestamp: number;
 }
 
-// Define a class to manage the system.
+// Defining a class to manage the system.
 class RealTimeAlertingSystem {
-  private db: Database; // Use your chosen database class
+  private db: Database; // Using chosen database class
   private syndicateData: Map<string, SyndicateInfo> = new Map();
   private transactionQueue: Queue<Transaction> = new Queue();
   private recentTransactions: Map<string, Transaction[]> = new Map();
@@ -20,34 +20,34 @@ class RealTimeAlertingSystem {
   }
 
   public processTransaction(transaction: Transaction): void {
-    // Add transaction to the queue
+    // Adding transaction to the queue
     this.transactionQueue.enqueue(transaction);
 
-   // Check if the transaction amount exceeds the predefined threshold
+   // Checking if the transaction amount exceeds the predefined threshold
 const syndicateInfo = this.syndicateData.get(transaction.syndicateId);
 if (syndicateInfo && transaction.amount > syndicateInfo.threshold) {
   AlertManager.triggerAlert('Threshold exceeded', transaction);
 }
 
-    // Maintain recent transactions data structure
+    // Maintaining recent transactions data structure
     this.recentTransactions
       .get(transaction.syndicateId)
       ?.push(transaction);
 
-    // Check for spikes in transaction rate
+    // Checking for spikes in transaction rate
     if (this.checkForSpike(transaction.syndicateId)) {
       AlertManager.triggerAlert('Transaction rate spike', transaction);
     }
   }
 
   private checkForSpike(syndicateId: string): boolean {
-    // Calculate the rate of transactions for the last hour
+    // Calculating the rate of transactions for the last hour
     const lastHour = this.getTransactionsWithinLastHour(
       this.recentTransactions.get(syndicateId) || []
     );
     const averageRate = this.calculateAverageRate(lastHour);
 
-    // Check for a spike in transaction rate
+    // Checking for a spike in transaction rate
     return (
       (this.recentTransactions.get(syndicateId) || []).length >
       10 * averageRate
@@ -57,7 +57,7 @@ if (syndicateInfo && transaction.amount > syndicateInfo.threshold) {
   private getTransactionsWithinLastHour(
     transactions: Transaction[]
   ): Transaction[] {
-    // Filter transactions within the last hour
+    // Filtering transactions within the last hour
     const oneHourAgo = Date.now() - 3600000; // 1 hour in milliseconds
     return transactions.filter(
       (transaction) => transaction.timestamp >= oneHourAgo
@@ -68,30 +68,31 @@ if (syndicateInfo && transaction.amount > syndicateInfo.threshold) {
     return transactions.length / 3600; // Transactions per second
   }
 
+  // Initializing data structures and start processing incoming transactions
   public initializeSystem(): void {
-    // Initialize data structures and start processing incoming transactions
-    // You can also set up database connections, alerting systems, etc.
+    
+    // We can also set up database connections, alerting systems, etc.
   }
 }
 
-// Define a class to represent syndicate information.
+// Defining a class to represent syndicate information.
 class SyndicateInfo {
   constructor(public threshold: number) {}
 }
 
-// Example database class (You should replace it with the actual database implementation).
+// Example database class.
 class Database {
-  // Methods to interact with your chosen database
+  // Methods to interact with chosen database
 }
 
-// Example AlertManager class (You should implement this class).
+// Example AlertManager class.
 class AlertManager {
   public static triggerAlert(type: string, transaction: Transaction): void {
-    // Implement alerting logic here
+    // alerting logic here
   }
 }
 
-// Initialize the system with a database instance.
+// Initializing the system with a database instance.
 const db = new Database();
 const alertingSystem = new RealTimeAlertingSystem(db);
 alertingSystem.initializeSystem();
